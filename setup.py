c634f689d972841b356666596fa746159dceb4d6
cloudhound <cloudhound.io>
2018-02-20 01:33:56 +0200

    setup.py changes

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name = "cftest",
-    version = "0.0.4",
+    version = "0.0.5",
     author = "cloudhound.io",
     author_email = "info@cloudhound.io",
-    description = ("self-contained security and best-practices testing-tool for Cloud Foundry environments"),
+    description = ("an open source security, best-practices and vulnerability scanner for Cloud Foundry"),
     license = "BSD",
-    keywords = "cf security health best practice test tester cloud foundry",
+    keywords = "cf security health best vulnerability scanner cloudfoundry practice test tester cloud foundry",
     url = "https://github.com/cloudhound-io/cftest",
     packages=['cftester'],
     install_requires=['requests'],
