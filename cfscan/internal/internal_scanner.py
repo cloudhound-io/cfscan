67ffbe74f4c829019f15400176ba949503ff4a56
cloudhound <36629245+cloudhound@users.noreply.github.com>
2018-03-05 15:48:35 +0200

    Change ETCDr to ETCDs

--- cfscan/internal/internal_scanner.py
+++ cfscan/internal/internal_scanner.py
@@ -225,7 +225,7 @@ class InternalScanner(Scanner):
         for status, msg in self.anonymous_access_to('ETCD', path='/v2/keys'):
             yield status, msg
             
-        for status, msg in self.anonymous_access_to('ETCD-R', path='/v2/version'):
+        for status, msg in self.anonymous_access_to('ETCD-S', path='/v2/members'):
             yield status, msg
 
 
